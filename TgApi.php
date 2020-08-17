a6a845062dad9c2d50fb254921311567b6d87a39
Vitaliy Olkhin <ovvitalik@gmail.com>
2020-08-17 15:02:20 +0500

    add curl, metod getMe

--- TgApi.php
+++ TgApi.php
@@ -10,7 +10,18 @@ namespace Tg;
 class TgApi {
     private $url = "https://api.telegram.org/bot";
     private $token = null;
+    public $chat_id = null; //id чата
+    public $username = null;
+    public $last_name = null;
+    public $first_name = null;
+    public $is_bot = null;
+    public $language_code = null;
     
+    /**
+     * Принимает телеграмм токен.
+     * 
+     * @param string $toket
+     */
     function __construct($toket) {
         $this->token = $toket;
     }
@@ -27,10 +38,45 @@ class TgApi {
         $arr = json_decode($json, true);
         
         if(is_array($arr)){
+            if(isset($arr['message'])){
+                $this->chat_id = $arr['message']['from']['id'];
+                $this->username = !empty($arr['message']['from']['username']) ? $arr['message']['from']['username'] : null;
+                $this->last_name = $arr['message']['from']['last_name'];
+                $this->is_bot = ($arr['message']['from']['is_bot'] === true) ? 1 : 0; //Если верно то бот
+                $this->language_code = $arr['message']['from']['language_code'];
+                $this->first_name = $arr['message']['from']['first_name'];
+            }
+            
             return $arr;
         }
         else {
             return false;
         }
+    }
+    
+    private function post(string $method="getMe", array $params=array(), string $get = null)
+    {
+        $ch = curl_init();
+        $getParams = empty($get) ? '' : '?' . $get;
+        curl_setopt($ch, CURLOPT_URL, $this->url . $this->token . "/" . $method . $getParams);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $output = curl_exec($ch);
+        \curl_close($ch);
+        
+        $arr = json_decode($output, true);
+        
+        return true;
+    }
+    
+    /**
+     * 
+     * @return array
+     */
+    public function getMe()
+    {
+        return $this->post();
     }
 }

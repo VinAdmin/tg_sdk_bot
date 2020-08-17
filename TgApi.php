3fb7df1276a07936f790557930484375160d3cbc
Vitaliy Olkhin <ovvitalik@gmail.com>
2020-08-17 12:50:39 +0500

    test repo

---
+++ TgApi.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Класс работы с Api для телеграмм бота TgApi
+ *
+ * @author Vitaliy Olkhin <ovvitalik@gmail.com>
+ */
+namespace Tg;
+
+class TgApi {
+    public function Token() {
+        return 'Yes';
+    }
+}
